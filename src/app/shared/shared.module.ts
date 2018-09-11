40c511bea95a35ef8eda8efa61dce09200674c0e
arslan <arslanafzal321@gmail.com>
2018-09-12 01:51:20 +0500

    primary header complete

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,17 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {RouterModule} from '@angular/router';
 
 import { PrimaryHeaderComponent } from './primary-header/primary-header.component';
 import { PrimaryFooterComponent } from './primary-footer/primary-footer.component';
 
+
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
   ],
   declarations: [PrimaryHeaderComponent, PrimaryFooterComponent],
   exports: [
     CommonModule,
-
+    RouterModule,
     PrimaryHeaderComponent,
     PrimaryFooterComponent,
   ]
