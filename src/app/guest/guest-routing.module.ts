7e1c86663ed748e5f407baa6ce0e32fe4f10c409
arslan <arslanafzal321@gmail.com>
2018-11-07 03:38:51 +0500

    added component register, forgot password, recover-password

--- src/app/guest/guest-routing.module.ts
+++ src/app/guest/guest-routing.module.ts
@@ -4,6 +4,9 @@ import { Routes, RouterModule } from '@angular/router';
 import {LayoutComponent} from './layout/layout.component';
 import {HomeComponent} from './home/home.component';
 import {LoginComponent} from './login/login.component';
+import {RegisterComponent} from './register/register.component';
+import {ForgotPasswordComponent} from './forgot-password/forgot-password.component';
+import {RecoverPasswordComponent} from './recover-password/recover-password.component';
 
 const routes: Routes = [
   {
@@ -17,6 +20,18 @@ const routes: Routes = [
       {
         path: 'login',
         component: LoginComponent
+      },
+      {
+        path: 'register',
+        component: RegisterComponent
+      },
+      {
+        path: 'forgot-password',
+        component: ForgotPasswordComponent
+      },
+      {
+        path: 'recover-password',
+        component: RecoverPasswordComponent
       }
     ]
   }
--- src/app/guest/guest.module.ts
+++ src/app/guest/guest.module.ts
@@ -5,12 +5,15 @@ import {SharedModule} from '../shared/shared.module';
 import { LayoutComponent } from './layout/layout.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+import { RecoverPasswordComponent } from './recover-password/recover-password.component';
 
 @NgModule({
   imports: [
     SharedModule,
     GuestRoutingModule,
   ],
-  declarations: [LayoutComponent, HomeComponent, LoginComponent]
+  declarations: [LayoutComponent, HomeComponent, LoginComponent, RegisterComponent, ForgotPasswordComponent, RecoverPasswordComponent]
 })
 export class GuestModule { }
